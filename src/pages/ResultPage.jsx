abb12d3edc6e02f0bee8832f6a1ce6d516895905
= <=>
2023-11-14 13:26:40 +0530

    update

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -22,19 +22,19 @@ export default function ResultPage() {
                     <h2 style={{color: 'var(--blue)'}}>Your score is <b style={{color: 'green'}}>{result.totalScore}</b></h2>
                     <table>
                         <tbody>
-                                <tr>
+                                <tr style={{color: 'blue'}}>
                                     <th>Total Questions</th>
                                     <td>{result.totalQuestions}</td>
                                 </tr>
-                                <tr>
+                                <tr style={{color: 'green'}}>
                                     <th>Correct Answers</th>
                                     <td>{result.correctAnswers}</td>
                                 </tr>
-                                <tr>
+                                <tr style={{color: 'red'}}>
                                     <th>Incorrect Answers</th>
                                     <td>{result.incorrectAnswers}</td>
                                 </tr>
-                                <tr>
+                                <tr style={{color: 'gray'}}>
                                     <th>Unanswered</th>
                                     <td>{result.totalQuestions-result.incorrectAnswers-result.correctAnswers}</td>
                                 </tr>
